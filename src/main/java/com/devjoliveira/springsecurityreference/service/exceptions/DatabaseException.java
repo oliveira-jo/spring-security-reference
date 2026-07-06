da5e260c5882e3aac6b66ea40de89fcd6ccdd347
Jonathan Oliveira <devjoliveira@gmail.com>
2026-07-06 20:17:41 -0300

    feat: create exeptions - database and resour not found

---
+++ src/main/java/com/devjoliveira/springsecurityreference/service/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package com.devjoliveira.springsecurityreference.service.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+  private static final long serialVersionUID = 1L;
+
+  public DatabaseException(String message) {
+    super(message);
+  }
+
+}
