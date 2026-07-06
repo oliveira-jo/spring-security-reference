89feee1499b6cb8189f1963b067f4d4486ffdbda
Jonathan Oliveira <devjoliveira@gmail.com>
2026-07-06 19:17:15 -0300

    first commit

---
+++ src/main/java/com/devjoliveira/springsecurityreference/SpringSecurityReferenceApplication.java
@@ -0,0 +1,13 @@
+package com.devjoliveira.springsecurityreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityReferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjoliveira/springsecurityreference/SpringSecurityReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjoliveira.springsecurityreference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityReferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
